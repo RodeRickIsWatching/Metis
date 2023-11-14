f538597e75580c3da5f8d718277962328f659684
roderick <roderick_lu@163.com>
2023-11-15 00:35:48 +0800

    feat

--- src/components/SequencerItemContainer/index.tsx
+++ src/components/SequencerItemContainer/index.tsx
@@ -1,5 +1,14 @@
+import fetchBatchBlockTx from '@/graphql/batchBlockAndTimestamp';
 import { getImageUrl } from '@/utils/tools';
+import { useRequest } from 'ahooks';
+import dayjs from 'dayjs';
+import { useEffect, useMemo } from 'react';
 import styled from 'styled-components';
+import duration from 'dayjs/plugin/duration';
+import relativeTime from 'dayjs/plugin/relativeTime';
+dayjs.extend(duration);
+dayjs.extend(relativeTime);
+
 
 const SequencerStatusContainer = styled.div`
   .avatar {
@@ -8,50 +17,64 @@ const SequencerStatusContainer = styled.div`
 `;
 
 const SequencerItemContainer = ({ ele, onClick, avatar, title, totalLockUp, uptime, since, earned }: any) => {
-    return (
-      <SequencerStatusContainer
-        onClick={onClick}
-        style={{ boxShadow: '0px 10px 30px 0px rgba(0, 0, 0, 0.10)' }}
-        className="pointer radius-30 w-340 pt-37 pl-30 pr-30 pb-20 flex flex-col gap-42 items-center position-relative"
-      >
-        <div className="flex flex-row items-center gap-8 position-absolute top-14 right-20">
-          <span className="fz-14 fw-500 color-00EA5E">Healthy</span>
-          <div className="s-12 bg-color-00EA5E radiusp-50" />
-        </div>
+  const { run, loading, data } = useRequest(fetchBatchBlockTx, { manual: true });
 
-        <div className="flex flex-col gap-5">
-          <div className={'avatar s-90 radiusp-50'}>{avatar ? <img src={avatar} /> : null}</div>
-          <div className="align-center fz-20 fw-700 poppins">{title}</div>
-        </div>
-        <div className="flex flex-col w-full gap-4">
-          <div className="flex flex-col gap-4">
-            <div className="flex flex-row justify-between items-center w-full">
-              <div className="fz-14 color-000 fw-400">Total Lock-up</div>
-              <div className="flex flex-row items-center gap-4">
-                <img src={getImageUrl('@/assets/images/sequencer/avatar.svg')} className="s-13" />
-                <div className="fz-14 fw-700 color-000">{totalLockUp || '-'}</div>
-              </div>
-            </div>
-            <div className="flex flex-row justify-between items-center w-full">
-              <div className="fz-14 color-000 fw-400">Uptime</div>
-              <div className="fz-14 fw-700 color-000">{uptime || '-'}</div>
-            </div>
-            <div className="flex flex-row justify-between items-center w-full">
-              <div className="fz-14 color-000 fw-400">Sequencing Since</div>
-              <div className="fz-14 fw-700 color-000">{since || '-'}</div>
-            </div>
-          </div>
-          <div className="h-1 w-full mt-10 mb-10 bg-color-CDCDCD" />
+  useEffect(() => {
+    if (ele?.user) {
+      run(ele?.user);
+    }
+  }, [ele?.user]);
+
+  const fromNow = useMemo(() => {
+    if(!data) return '-'
+    const _duration = data - +dayjs().unix();
+    return dayjs.duration(_duration, 's').humanize(true);
+  }, [data]);
+
+  return (
+    <SequencerStatusContainer
+      onClick={onClick}
+      style={{ boxShadow: '0px 10px 30px 0px rgba(0, 0, 0, 0.10)' }}
+      className="pointer radius-30 w-340 pt-37 pl-30 pr-30 pb-20 flex flex-col gap-42 items-center position-relative"
+    >
+      <div className="flex flex-row items-center gap-8 position-absolute top-14 right-20">
+        <span className="fz-14 fw-500 color-00EA5E">Healthy</span>
+        <div className="s-12 bg-color-00EA5E radiusp-50" />
+      </div>
+
+      <div className="flex flex-col gap-5">
+        <div className={'avatar s-90 radiusp-50'}>{avatar ? <img src={avatar} /> : null}</div>
+        <div className="align-center fz-20 fw-700 poppins">{title}</div>
+      </div>
+      <div className="flex flex-col w-full gap-4">
+        <div className="flex flex-col gap-4">
           <div className="flex flex-row justify-between items-center w-full">
-            <div className="fz-14 color-000 fw-400">Earned</div>
-
+            <div className="fz-14 color-000 fw-400">Total Lock-up</div>
             <div className="flex flex-row items-center gap-4">
               <img src={getImageUrl('@/assets/images/sequencer/avatar.svg')} className="s-13" />
-              <div className="fz-14 fw-700 color-000">{ele?.rewardReadable || '-'}</div>
+              <div className="fz-14 fw-700 color-000">{totalLockUp || '-'}</div>
             </div>
           </div>
+          <div className="flex flex-row justify-between items-center w-full">
+            <div className="fz-14 color-000 fw-400">Uptime</div>
+            <div className="fz-14 fw-700 color-000">{fromNow || '-'}</div>
+          </div>
+          <div className="flex flex-row justify-between items-center w-full">
+            <div className="fz-14 color-000 fw-400">Sequencing Since</div>
+            <div className="fz-14 fw-700 color-000">{since || '-'}</div>
+          </div>
         </div>
-      </SequencerStatusContainer>
-    );
-  };
+        <div className="h-1 w-full mt-10 mb-10 bg-color-CDCDCD" />
+        <div className="flex flex-row justify-between items-center w-full">
+          <div className="fz-14 color-000 fw-400">Earned</div>
+
+          <div className="flex flex-row items-center gap-4">
+            <img src={getImageUrl('@/assets/images/sequencer/avatar.svg')} className="s-13" />
+            <div className="fz-14 fw-700 color-000">{ele?.rewardReadable || '-'}</div>
+          </div>
+        </div>
+      </div>
+    </SequencerStatusContainer>
+  );
+};
 export default SequencerItemContainer;
---
+++ src/graphql/batchBlockAndTimestamp.ts
@@ -0,0 +1,58 @@
+// 获取最近出的区块和时间
+// todo 优化
+import { blockGraphUrl } from '@/configs/common';
+import { gql, GraphQLClient } from 'graphql-request';
+
+// "add1": "0xfe08ee83b1f01d6d7c6eff3c8c84fa6fe02fca17",
+// "add2": "0x1267397fb5bf6f6dcc3d18d673616d512dbcd8f0",
+// "add3": "0x3eb630c3c267395fee216b603a02061330d39642",
+
+const userTxs = gql`
+  query MyQuery($address: String) {
+    userEpochParams(first: 5, orderDirection: desc, orderBy: epochId, where: { signer: $address }) {
+      signer
+      id
+      epochId
+      endBlock
+      startBlock
+      blockTimestamp
+    }
+  }
+`;
+
+const blocks = gql`
+  query MyQuery($from: String, $to: String) {
+    blocks(first: 1, orderDirection: desc, orderBy: number, where: { number_gte: $from, number_lte: $to }) {
+      number
+      timestamp
+    }
+  }
+`;
+
+const perpetualClient = new GraphQLClient(blockGraphUrl, {
+  headers: {},
+});
+
+const fetchBatchBlockTx = async (address: string) => {
+  if (!address) return null;
+  const _address = address.toString().toLowerCase();
+  const txData: any = await perpetualClient.request(userTxs, {
+    address: _address,
+  });
+
+  const startBlock = txData?.userEpochParams?.[txData?.userEpochParams?.length - 1]?.startBlock;
+  const endBlock = txData?.userEpochParams?.[0]?.endBlock;
+  if (!endBlock || !startBlock) return undefined;
+
+  // 查询block服务查询时间
+  const blockData: any = await perpetualClient.request(blocks, {
+    from: startBlock,
+    to: endBlock,
+  });
+
+  const timestamp = blockData?.blocks?.[0]?.timestamp;
+
+  return timestamp;
+};
+
+export default fetchBatchBlockTx;
